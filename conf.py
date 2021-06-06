1bbe1b364adeb11b399355cc3ab4292b9006526a
Zachary Dodge <zacharysdodge@gmail.com>
2021-06-06 11:39:21 -0700

    Add nojekyll to build output

--- conf.py
+++ conf.py
@@ -37,6 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'example', 'images']
 
 
 # -- Options for HTML output -------------------------------------------------
+html_extra_path = ['.nojekyll']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
